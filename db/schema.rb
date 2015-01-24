c745160e705e2d51810869e5d067c8118d4e60f2
GustavoCaso <gustavocaso@gmail.com>
2015-01-24 13:37:06 +0100

    Added new schema postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140523122100) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "newsletters", force: true do |t|
     t.string   "title"
     t.text     "text"
