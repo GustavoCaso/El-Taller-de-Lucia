2293fa8ab526475942b9ce68d482714c3cf564f8
GustavoCaso <gustavocaso@gmail.com>
2015-01-28 08:21:21 +0100

    Added more changes

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  //  To animate after cliking the scroll down button to the element minus the padding
+  $('.scroll_down').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.servicios').offset().top - 40},
+    'slow');
+  }) ;
+
+
   $("#layerslider").layerSlider({
     skinsPath: 'assets/skins/',
     skin: 'glass',
